bec1fdb13735bb9ddb37fb760ac7f2aacc03f925
Ansu-dev <ansu1007@naver.com>
2025-03-07 16:11:14 +0900

    ✨ Feat: two-pointer solved

--- TwoPointer/sub_of_two_numbers.py
+++ TwoPointer/sub_of_two_numbers.py
@@ -1,6 +1,20 @@
 
 # n: 수열의 길이, x: target 숫자, sequences: n길이의 수열
-def solution(n, x, sequence):
-    return
+def solution(target, sequence):
+    answer = 0
+    # 두수의 합이 target을 만족하는 쌍이 몇개가 있는지
+    left,right = 0, len(sequence) - 1
+    sequence.sort()
+    print(sequence)
+    while left < right:
+        sum_number = sequence[left] + sequence[right]
+        if sum_number == target:
+            answer += 1
+            right -= 1
+        elif sum_number < target:
+            left += 1
+        else:
+            right -= 1
+    return answer
 
-print(solution(9,13, [5,12,7,10,9,1,2,3,11]))
+print(solution(13, [5,12,7,10,9,1,2,3,11]))
--- TwoPointer/sub_total.py
+++ TwoPointer/sub_total.py
@@ -6,7 +6,25 @@
 # 입력
 # 첫째 줄에 N (10 ≤ N < 100,000)과 S (0 < S ≤ 100,000,000)가 주어진다. 둘째 줄에는 수열이 주어진다. 수열의 각 원소는 공백으로 구분되어져 있으며, 10,000이하의 자연수이다.
 
-def solution(N, S, sequence):
-    return
+def solution(target, sequence):
+    answer = len(sequence) + 1
+    left,right = 0, 0 # 투 포인터를 배열의 시작점에 위치
+    current_sum = 0 # 현재 윈도우의 합을 저장
 
-print(solution(10,15, [5,1,3,5,10,7,4,9,2,8]))
+    while True:
+        if current_sum >= target:
+            # 현재 저장된 길이와 target이 넘는 부분합의 길이중 더 짧은것을 저장
+            answer = min(answer, right - left) # right는 범위에 포함되지 않으므로 +1을 안더하는게 맞다
+
+            # 더 짧은 길이를 구하기 위해 left를 확장
+            current_sum -= sequence[left]
+            left += 1
+        elif right == len(sequence): # right가 배열의 끝인덱스를 벗어났을 때
+            break
+        else:
+            # 윈도우를 확장하기 위해 right 포인터 이동
+            current_sum += sequence[right]
+            right += 1
+    return answer
+
+print(solution(15, [5,1,3,5,10,7,4,9,2,8]))
